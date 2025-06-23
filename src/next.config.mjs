1e4f7d5b475914441572cd05ea69b36274aebf72
Cedric Yovodevi <sales@edxstore.com>
2025-06-23 05:35:19 +0000

    If I clicked on the Open in New Window, it is not working it creates thi

---
+++ src/next.config.mjs
@@ -0,0 +1,34 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  allowedDevOrigins: [
+    'http://localhost:9003',
+    'http://10.88.0.3:9003',
+    'https://ssh.cloud.google.com',
+  ],
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
