d44f0a1cc9880652efecf111227898f2b442df41
Cedric Yovodevi <sales@edxstore.com>
2025-06-23 05:37:27 +0000

    It is still now working

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: [
     'http://localhost:9003',
     'http://10.88.0.3:9003',
+    'https://*.ssh.cloud.google.com',
   ],
   /* config options here */
   typescript: {
@@ -19,6 +20,10 @@ const nextConfig: NextConfig = {
         hostname: 'placehold.co',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
       },
     ],
   },
